5dd2bb8bc84e9de6220b158e429ac209adb60001
avalenza <alesvale97@gmail.com>
2026-07-05 19:16:14 +0300

    serve.sh and .gitattributes

--- attn_explorer/script/serve.sh
+++ attn_explorer/script/serve.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 uv run python -m attn_explorer.serve \
-      --store "$1"
+      --host 0.0.0.0 --port 5001 \
+      --store data/vnbench/output_v4 \
+      --videos data/vnbench/videos/v4
